443b5ae6265bc72cd5d6dd71ce7dac021e7bcc61
ms007 <ms007@gmx.ch>
2018-04-23 21:25:47 +0200

    revice hero saga

--- src/state/hero/sagas.js
+++ src/state/hero/sagas.js
@@ -1,26 +1,26 @@
-import { put, takeLatest } from 'redux-saga/effects';
+import { put, call, takeLatest } from 'redux-saga/effects';
 import get from 'lodash/fp/get';
 
 import { heroService } from 'services';
-import types from './types';
 import actions from './actions';
 
 function* fetchHero() {
   try {
     yield put(actions.loadingHero(true));
-    const { code, data } = yield heroService.get('hulk');
+    const { code, data } = yield call(heroService.get, 'hulk');
     const hero = get('results[0]', data);
     if (code === 200) {
       yield put(actions.addHero(hero));
     }
   } catch (e) {
     console.log(e);
+  } finally {
+    yield put(actions.loadingHero(false));
   }
-  yield put(actions.loadingHero(false));
 }
 
 function* watchFetchHero() {
-  yield takeLatest(types.FETCH_HERO, fetchHero);
+  yield takeLatest(actions.fetchHero, fetchHero);
 }
 
 export default {
